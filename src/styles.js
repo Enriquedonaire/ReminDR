0dab232e8d1ab602d119e0464250168bc39f15aa
Enrique Donaire <donaire.q2@gmail.com>
2021-10-16 13:55:42 +0200

    First commit ReminDr

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+    appBar: {
+        backgroundColor:'#030c0d',
+        borderRadius: 15,
+        margin: '0 0 0 0',
+        marginBottom: '90px',
+        padding: '30px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    heading: {
+        color: 'rgba(0,183,255, 1)',
+    },
+    image: {
+        marginLeft: '15px',
+    },
+
+}));
+
+
